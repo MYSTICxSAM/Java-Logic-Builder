ae2649f8b59180b4cf4d5f2ae3320e01949ad810
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-08-16 21:06:57 +0530

    Update Fibonacci Series.java

--- Fibonacci Series.java
+++ Fibonacci Series.java
@@ -5,7 +5,7 @@ class Fibonacci_series
 {
     public static void main(String args[])
     {
-        System.out.print("Enter a number:");
+        System.out.print("Enter a count for Fibonacci Series:");
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
         int x = 0;
