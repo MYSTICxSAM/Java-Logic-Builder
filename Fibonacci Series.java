cb6147e924f080a445b73c7f095ffead60fc1283
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-08-16 21:05:10 +0530

    Update Fibonacci Series.java
    Uploaded code done on vs code

--- Fibonacci Series.java
+++ Fibonacci Series.java
@@ -1,1 +1,22 @@
 // In fibonacci series, next number is the sum of previous two numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc. The first two numbers of fibonacci series are 0 and 1.
+
+import java.util.*;
+class Fibonacci_series
+{
+    public static void main(String args[])
+    {
+        System.out.print("Enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int x = 0;
+        int y = 1;
+        System.out.print("Fibonacci: 0 " );
+        for(int i = 0 ; i<num ; i++)
+        {
+            int fibb = x+y;
+            System.out.print(fibb + " ");
+            y = x;
+            x = fibb;
+        }
+    }
+}
