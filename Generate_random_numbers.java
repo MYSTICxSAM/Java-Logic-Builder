5b4795cd584f84311ab19a29b1168a73b38838ea
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-08-19 00:02:34 +0530

    Add files via upload

---
+++ Generate_random_numbers.java
@@ -0,0 +1,8 @@
+//Random numbers are the numbers that use a large set of numbers and selects a number using the mathematical algorithm.
+public class Generate_random_numbers
+{
+    public static void main()//math.random
+    {
+        System.out.println("Ramdom Number is:" + Math.random() );
+    }    
+}
