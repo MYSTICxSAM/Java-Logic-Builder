e4edbdab89651f83374f82bc3ba638982ee73911
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-08-28 23:11:43 +0530

    Add files via upload

---
+++ 1.Basic of Java/Odd_0_to_100.java
@@ -0,0 +1,13 @@
+public class Odd_0_to_100 
+{
+    public static void main(String args[])
+    {
+        for(int i=1;i<=100;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
