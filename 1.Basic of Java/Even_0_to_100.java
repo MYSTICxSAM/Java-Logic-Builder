16fa07ebf4ece9b2d64aa2332ed0795e691647b8
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-08-28 23:10:21 +0530

    Add files via upload

---
+++ 1.Basic of Java/Even_0_to_100.java
@@ -0,0 +1,14 @@
+public class Even_0_to_100 
+{
+    public static void main(String args[])
+    {
+        for(int i=2;i<=100;i++)
+        {
+            if(i%2==0)
+            {
+                System.out.print(i + " ");
+            }
+        }
+    }
+        
+}
