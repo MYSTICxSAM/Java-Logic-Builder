62fc4ae0e801dca1caa7c6ef34982a43266a9175
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-08-29 23:04:53 +0530

    Add files via upload

---
+++ 1.Basic of Java/Sum_of_Natural_Number.java
@@ -0,0 +1,14 @@
+public class Sum_of_Natural_Number {
+    public static void main(String[] args) 
+    {
+        int i, num = 10, sum = 0;
+        
+        for (i = 1; i <= num; ++i) 
+        {
+            
+            sum = sum + i;
+        }
+        // prints the sum
+        System.out.println("Sum of First 10 Natural Numbers is = " + sum);
+    }
+}
