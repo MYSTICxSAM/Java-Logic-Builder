f1b401e8427c86593b375bf872f9910302468cae
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-09-03 23:11:01 +0530

    Add files via upload

---
+++ 2.Array/Creating_and_Printing.java
@@ -0,0 +1,21 @@
+package Array;
+import java.util.*;
+public class Creating_and_Printing
+{
+    public static void main(String[] args) 
+    {
+        int[] numbers = new int[10]; //array of integers with a size of 10
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter 10 Elements:");
+        for (int i = 0; i < numbers.length; i++) 
+        {
+            numbers[i] =  sc.nextInt();//Fill the array with numbers from 1 to 10
+        }
+        System.out.print("Array elements are:");
+        for (int number : numbers)// Print the array elements 
+        {
+            System.out.print(number + " ");
+        }
+    }
+
+}
