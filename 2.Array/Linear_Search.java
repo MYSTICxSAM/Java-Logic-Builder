67dbbd8806dacab6e39e2ddf3c290837b54c92ba
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-09-07 17:59:59 +0530

    Add files via upload

---
+++ 2.Array/Linear_Search.java
@@ -0,0 +1,24 @@
+package Array;
+import java.util.*;
+public class Linear_Search 
+{
+    public static void main(String args[])
+    {
+        System.out.print("Enter 5 numbers in assending order: ");
+        Scanner sc =  new Scanner(System.in);
+        int num[] = new int[5];
+        for(int i= 0;i<num.length;i++)
+        {
+            num[i] = sc.nextInt();
+        }
+        System.out.print("Enter a number to be searched: ");
+        int search = sc.nextInt();
+        for(int i = 0;i<num.length;i++)
+        {
+            if(search==num[i])
+            {
+                System.out.print(num[i] + " is on index " + i);
+            }
+        }
+    }
+}
