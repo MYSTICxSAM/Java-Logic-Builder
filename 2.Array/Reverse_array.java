3c750cd22d00d672f6a710b5a7fd67681f84fac1
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-09-14 21:46:20 +0530

    Add files via upload

---
+++ 2.Array/Reverse_array.java
@@ -0,0 +1,26 @@
+package Array;
+import java.util.*;
+public class Reverse_array 
+{
+    public static void main(String args[])
+    {
+        System.out.print("Enter the elements of array: ");
+        int n[] = new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i = 0;i<n.length;i++)
+        {
+            n[i] = sc.nextInt();
+        }
+        System.out.print("Reverse of the array is: ");
+        int n1[] = new int[5];
+        for(int i = 0;i<n.length;i++)
+        {
+            n1[i] = n[n.length-i - 1];
+        }
+        for(int i = 0;i<n.length;i++)
+        {
+            System.out.print(n1[i] + " ");
+        }
+
+    }
+}
