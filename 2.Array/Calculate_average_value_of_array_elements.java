29ecacca4325f8cf2309faeddb16cf1c1722e73d
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-09-20 23:35:06 +0530

    Add files via upload

---
+++ 2.Array/Calculate_average_value_of_array_elements.java
@@ -0,0 +1,19 @@
+package Array;
+
+public class Calculate_average_value_of_array_elements 
+{
+    
+public class Exercise4 {
+    
+    public static void main(String[] args) 
+    {  
+        int[] numbers = new int[]{20, 30, 25, 35, -16, 60, -100};
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum = sum + numbers[i];
+        }
+        double average = sum / numbers.length;
+        System.out.println("Average value of the array elements is : " + average); 
+    }
+}
+}
