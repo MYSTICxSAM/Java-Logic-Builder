635f863dd80c7ebfdfc677909e81213545e82835
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-10-11 21:59:15 +0530

    Add files via upload

---
+++ 3.Sorting/Sort.java
@@ -0,0 +1,10 @@
+package Sorting;
+import java.util.Arrays;
+public class Sort{
+    public static void main(String args[])
+    {
+        int arr[] = { 5, -2, 23, 7, 87, -42, 509 };
+        Arrays.sort(arr);
+        System.out.println(arr[1]);
+    }
+}
