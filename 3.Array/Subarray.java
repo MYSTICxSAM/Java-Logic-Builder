3cd0077ce389a251d3a174231faab820380182bb
Samarth Prashar <135192183+MYSTICxSAM@users.noreply.github.com>
2024-12-19 18:02:18 +0530

    Add files via upload

---
+++ 3.Array/Subarray.java
@@ -0,0 +1,20 @@
+public class Subarray 
+{
+    public static void main(String args[])
+    {
+        int arr[] = {2,4,7,1,9};
+
+        for(int i=0; i<arr.length;i++)
+        {
+            for(int j=i; j<arr.length;j++)
+            {
+                for(int k=i;k<=j;k++)
+                {
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+    }    
+}
